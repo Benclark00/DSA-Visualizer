ed1ed8ee931d1ac78c91f8bef8d96f0b0fd78a1f
Ben Clark <clarkb5@mymail.vcu.edu>
2024-09-13 09:11:06 -0400

    creation of arrays component

---
+++ dsa-visualizer/src/app/arrays/arrays.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrays',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrays.component.html',
+  styleUrl: './arrays.component.css'
+})
+export class ArraysComponent {
+
+}
